8f8f16b5faa44b20b122cbc18cc8c4cd7d4a0ad0
Abhinandh Ajay <abhinandhajay@gmail.com>
2021-11-01 09:55:56 -0700

    Update hero description text

--- src/views/LandingPage/LandingPage.js
+++ src/views/LandingPage/LandingPage.js
@@ -53,8 +53,10 @@ export default function LandingPage(props) {
                 The new way of connecting with CMU.
               </h1>
               <h4>
-                We care about you, <br /> would do anything for you, <br /> and
-                hope you would gain love and fame someday.
+                {/* Need to change to something else if necessary */}
+                <strong>Connect</strong> with people. <br />{" "}
+                <strong>Grow</strong> your network. <br /> <strong>Earn</strong>{" "}
+                NFTs along the way.
               </h4>
               <br />
               <Box className={classes.subscribeBox}>
