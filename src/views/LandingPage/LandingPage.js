1294bccde3e1e19caf26f5c9358e56a074fbd5df
Abhinandh Ajay <abhinandhajay@gmail.com>
2021-11-01 14:51:22 -0700

    Update text

--- src/views/LandingPage/LandingPage.js
+++ src/views/LandingPage/LandingPage.js
@@ -54,7 +54,7 @@ export default function LandingPage(props) {
               </h1>
               <h4>
                 {/* Need to change to something else if necessary */}
-                <strong>Connect</strong> with people. <br />{" "}
+                <strong>Connect</strong> with your peers and alumni. <br />{" "}
                 <strong>Grow</strong> your network. <br /> <strong>Earn</strong>{" "}
                 NFTs along the way.
               </h4>
